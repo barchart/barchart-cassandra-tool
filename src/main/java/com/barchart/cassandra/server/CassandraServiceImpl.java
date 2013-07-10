358390f5ce86f3109fadac0395ad765a4dd6dd13
mjsokolowski <m-sokolowski@SOKOLOWSKI-PC.bcinc.local>
2013-07-10 12:19:40 -0500

    First steps with Astyanax

--- src/main/java/com/barchart/cassandra/server/CassandraServiceImpl.java
+++ src/main/java/com/barchart/cassandra/server/CassandraServiceImpl.java
@@ -1,8 +1,18 @@
 package com.barchart.cassandra.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.barchart.cassandra.client.CassandraService;
 import com.barchart.cassandra.shared.FieldVerifier;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.netflix.astyanax.AstyanaxContext;
+import com.netflix.astyanax.Keyspace;
+import com.netflix.astyanax.connectionpool.NodeDiscoveryType;
+import com.netflix.astyanax.connectionpool.impl.ConnectionPoolConfigurationImpl;
+import com.netflix.astyanax.connectionpool.impl.CountingConnectionPoolMonitor;
+import com.netflix.astyanax.impl.AstyanaxConfigurationImpl;
+import com.netflix.astyanax.thrift.ThriftFamilyFactory;
 
 /**
  * The server side implementation of the RPC service.
@@ -11,14 +21,20 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 public class CassandraServiceImpl extends RemoteServiceServlet implements
 		CassandraService {
 
+	protected final Logger log = LoggerFactory.getLogger(getClass());
+
+	private AstyanaxContext<Keyspace> context = null;
+	private Keyspace keyspace = null;
+
 	public String greetServer(String input) throws IllegalArgumentException {
+
 		// Verify that the input is valid.
 		if (!FieldVerifier.isValidName(input)) {
+
 			// If the input is not valid, throw an IllegalArgumentException back
 			// to
 			// the client.
-			throw new IllegalArgumentException(
-					"Name must be at least 4 characters long");
+			throw new IllegalArgumentException("Name must be a valid address");
 		}
 
 		String serverInfo = getServletContext().getServerInfo();
@@ -29,8 +45,32 @@ public class CassandraServiceImpl extends RemoteServiceServlet implements
 		input = escapeHtml(input);
 		userAgent = escapeHtml(userAgent);
 
-		return "Hello, " + input + "!<br><br>I am running " + serverInfo
-				+ ".<br><br>It looks like you are using:<br>" + userAgent;
+		try {
+			context = new AstyanaxContext.Builder()
+					.forCluster("ClusterName")
+					.forKeyspace("KeyspaceName")
+					.withAstyanaxConfiguration(
+							new AstyanaxConfigurationImpl()
+									.setDiscoveryType(NodeDiscoveryType.RING_DESCRIBE))
+					.withConnectionPoolConfiguration(
+							new ConnectionPoolConfigurationImpl(
+									"MyConnectionPool").setPort(9160)
+									.setMaxConnsPerHost(1)
+									.setSeeds(input + ":9160"))
+					.buildKeyspace(ThriftFamilyFactory.getInstance());
+
+			context.start();
+			keyspace = context.getClient();
+
+			if (context != null)
+				return "Successfully connected to " + input;
+
+			return "Problems connecting to " + input;
+
+		} catch (Exception e) {
+			log.error( "Fatal error", e );
+			return "Problems:\n" + e.getStackTrace();
+		}
 	}
 
 	/**
