a21ba4e1f27b94d08f881481b66cb50390f6174d
mjsokolowski <m-sokolowski@SOKOLOWSKI-PC.bcinc.local>
2013-07-10 09:43:48 -0500

    init

---
+++ src/main/java/com/barchart/cassandra/client/CassandraService.java
@@ -0,0 +1,12 @@
+package com.barchart.cassandra.client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+/**
+ * The client side stub for the RPC service.
+ */
+@RemoteServiceRelativePath("greet")
+public interface CassandraService extends RemoteService {
+	String greetServer(String name) throws IllegalArgumentException;
+}
